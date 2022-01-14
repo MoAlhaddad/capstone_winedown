6e5dc378a93e4862043ffec8c92784a2341d84e4
Mohamad Alhaddad <52386724+MoAlhaddad@users.noreply.github.com>
2022-01-13 19:25:19 -0800

    Update Paginate.test.js

--- src/Paginate.test.js
+++ src/Paginate.test.js
@@ -1,14 +1,18 @@
-import * as React from "react";
+import * as React from 'react';
 // import testing utilities
-import Paginate from "./components/Paginate";
+import Paginate from "./Paginate";
 import wineList from "./wineList.json";
-import renderer from "react-test-renderer";
+import renderer from 'react-test-renderer';
 
 describe("Paginate", () => {
   it("rendered Pagination", () => {
+    const assumedWineListLength = 51;
     const component = renderer.create(
-      <Paginate wineList={wineList.data} displayFavoriteWineColumn={true} />
+     <Paginate wineList={wineList.data}  displayFavoriteWineColumn={true} />
     );
+    //Check the list items length are equal to 51
+    expect(component.root.props.wineList.length).toEqual(assumedWineListLength);
+    // expect(component.root.f)
     expect(component).toMatchSnapshot();
   });
 });
